ad463c70994f2665f1219837bfcd61ef87581721
Emil Kristensen <152981934+taro-dales@users.noreply.github.com>
2023-12-07 12:22:58 +0700

    Create VowelManager.hpp

---
+++ VowelManager.hpp
@@ -0,0 +1,12 @@
+#ifndef VOWEL_MANAGER_HPP
+#define VOWEL_MANAGER_HPP
+
+#include "NamePart.hpp"
+#include <initializer_list>
+
+class VowelManager : public NamePart {
+public:
+    VowelManager();
+};
+
+#endif // VOWEL_MANAGER_HPP
