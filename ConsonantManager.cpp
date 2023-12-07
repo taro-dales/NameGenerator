a5fb8d75a7910375d73464bd29efa837f668bca1
Emil Kristensen <152981934+taro-dales@users.noreply.github.com>
2023-12-07 12:23:43 +0700

    Create ConsonantManager.cpp

---
+++ ConsonantManager.cpp
@@ -0,0 +1,3 @@
+#include "ConsonantManager.hpp"
+
+ConsonantManager::ConsonantManager() : NamePart(), elements{"b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"} {}
