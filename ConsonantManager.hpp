8ee452fd6d7cf81ea2459e697387446859c098ea
Emil Kristensen <152981934+taro-dales@users.noreply.github.com>
2023-12-07 12:23:22 +0700

    Create ConsonantManager.hpp

---
+++ ConsonantManager.hpp
@@ -0,0 +1,12 @@
+#ifndef CONSONANT_MANAGER_HPP
+#define CONSONANT_MANAGER_HPP
+
+#include "NamePart.hpp"
+#include <initializer_list>
+
+class ConsonantManager : public NamePart {
+public:
+    ConsonantManager();
+};
+
+#endif // CONSONANT_MANAGER_HPP
